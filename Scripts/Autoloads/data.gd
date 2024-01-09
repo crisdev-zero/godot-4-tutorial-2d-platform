70a28338af96d81e54227f6791851847a6fc581f
Thomas Yanuziello <thomas.yanuziello@gmail.com>
2024-01-09 08:34:12 -0500

    Sections 1-4 Complete

---
+++ Scripts/Autoloads/data.gd
@@ -0,0 +1,10 @@
+class_name Data extends Resource
+
+@export var coins : int
+@export var lives : int
+@export var has_key : bool
+
+func _init():
+	coins = 0
+	lives = 3
+	has_key = false
---
+++ Scripts/Autoloads/file.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var data : Data
+
+#temporary
+func _ready():
+	new_game()
+
+func new_game():
+	data = Data.new()
+
+func save_game():
+	pass
+
+func load_game():
+	pass
