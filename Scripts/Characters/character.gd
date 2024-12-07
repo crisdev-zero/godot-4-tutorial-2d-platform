159631d84afed70ec97b47735f703480ae1cfe3c
crisdev <cristian.sprohnle@gmail.com>
2024-12-07 14:23:10 -0300

    fix: roger: fix several roger bugs
    - allow character to move when attacking via property
    - fix ground and air attack effects rendering before attacking
    - fix null pointer to start_cooldown method
    - fix roger attacking loop
    - fix roger hit animation

--- Scripts/Characters/character.gd
+++ Scripts/Characters/character.gd
@@ -1,59 +1,65 @@
 class_name Character extends CharacterBody2D
 
-var _is_bound : bool
-var _min : Vector2
-var _max : Vector2
+var _is_bound: bool
+var _min: Vector2
+var _max: Vector2
 
 @export_category("Locomotion")
-@export var _speed : float = 8
-@export var _acceleration : float = 16
-@export var _deceleration : float = 32
+@export var _speed: float = 8
+@export var _acceleration: float = 16
+@export var _deceleration: float = 32
 
 @export_category("Jump")
-@export var _jump_height : float = 2.5
-@export var _air_control : float = 0.5
-@export var _jump_dust : PackedScene
-var _jump_velocity : float
-var _was_on_floor : bool
+@export var _jump_height: float = 2.5
+@export var _air_control: float = 0.5
+@export var _jump_dust: PackedScene
+var _jump_velocity: float
+var _was_on_floor: bool
 
 @export_category("Sprite")
-@export var _is_facing_left : bool
-@export var _sprites_face_left : bool
-@onready var _sprite : Sprite2D = $Sprite2D
+@export var _is_facing_left: bool
+@export var _sprites_face_left: bool
+@onready var _sprite: Sprite2D = $Sprite2D
 
 @export_category("Swim")
-@export var _density : float = -0.1
-@export var _drag : float = 0.5
-var _water_surface_height : float
-var _is_in_water : bool
-var _is_below_surface : bool
+@export var _density: float = -0.1
+@export var _drag: float = 0.5
+var _water_surface_height: float
+var _is_in_water: bool
+var _is_below_surface: bool
 
 @export_category("Combat")
-@export_range(1, 100) var _max_health : int = 5
-@export_range(0, 5) var _invincible_duration : float = 0
-@export_range(0, 5) var _attack_damage : int = 1
-@export_range(0, 10) var _stagger : float = 5
-@export var _is_hit : bool
-@export var _is_dead : bool
-@export var _wants_to_attack : bool
-@export var _is_attacking : bool
-@onready var _current_health : int = _max_health
-@onready var _hurt_box : Area2D = $HurtBox
-@onready var _hit_box : Area2D = get_node_or_null("HitBox")
-var _invincible_time : Timer
+@export_range(1, 100) var _max_health: int = 5
+@export_range(0, 5) var _invincible_duration: float = 0
+@export_range(0, 5) var _attack_damage: int = 1
+@export_range(0, 10) var _stagger: float = 5
+## Indicates if the character will stop moving when attacking
+@export var _stop_moving: bool = true
+@export var _is_hit: bool:
+	set = set_is_hit
+@export var _is_dead: bool
+@export var _wants_to_attack: bool:
+	set = set_wants_to_attack
+@export var _is_attacking: bool:
+	set = set_is_attacking
+@onready var _current_health: int = _max_health
+@onready var _hurt_box: Area2D = $HurtBox
+@onready var _hit_box: Area2D = get_node_or_null("HitBox")
+var _invincible_time: Timer
 
-var _collision_layer : int = collision_layer
-var _collision_mask : int = collision_mask
+var _collision_layer: int = collision_layer
+var _collision_mask: int = collision_mask
 
-signal changed_direction(is_facing_left : bool)
-signal landed(floor_height : float)
-signal health_changed(percentage : float)
-signal died()
+signal changed_direction(is_facing_left: bool)
+signal landed(floor_height: float)
+signal health_changed(percentage: float)
+signal died
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-var _direction : float
+var _direction: float
+
 
 func _ready():
 	_speed *= Global.ppt
@@ -68,15 +74,29 @@ func _ready():
 		_hit_box.monitoring = false
 	_is_attacking = false
 
+
 #region Public Methods
+func set_wants_to_attack(value: bool):
+	_wants_to_attack = value
+
+
+func set_is_attacking(value: bool):
+	_is_attacking = value
+
+
+func set_is_hit(value: bool):
+	_is_hit = value
+
 
 func is_facing_left() -> bool:
 	return _is_facing_left
 
+
 func attack():
 	_wants_to_attack = true
 
-func take_damage(amount : int, direction : Vector2):
+
+func take_damage(amount: int, direction: Vector2):
 	_current_health = max(_current_health - amount, 0)
 	health_changed.emit(float(_current_health) / _max_health)
 	velocity = direction * Global.ppt * _stagger
@@ -89,24 +109,28 @@ func take_damage(amount : int, direction : Vector2):
 		if _invincible_duration != 0:
 			become_invincible(_invincible_duration)
 
-func recover(amount : int):
+
+func recover(amount: int):
 	_current_health = min(_current_health + amount, _max_health)
 	health_changed.emit(float(_current_health) / _max_health)
 
-func become_invincible(duration : float):
+
+func become_invincible(duration: float):
 	_hurt_box.set_deferred("monitorable", false)
 	_invincible_time.start(duration)
 	await _invincible_time.timeout
 	_hurt_box.monitorable = true
 
-func set_bounds(min_boundary : Vector2, max_boundary : Vector2):
+
+func set_bounds(min_boundary: Vector2, max_boundary: Vector2):
 	_is_bound = true
 	_min = min_boundary
 	_max = max_boundary
-	var sprite_size : Vector2 = _sprite.get_rect().size
+	var sprite_size: Vector2 = _sprite.get_rect().size
 	_min.x += sprite_size.x / 2
 	_max.x -= sprite_size.x / 2
 	_min.y += sprite_size.y
+
 
 func face_left():
 	if _is_dead || _is_attacking:
@@ -117,20 +141,23 @@ func face_left():
 		_hit_box.scale.x = 1 if _sprites_face_left else -1
 	changed_direction.emit(_is_facing_left)
 
+
 func face_right():
 	if _is_dead || _is_attacking:
 		return
 	_is_facing_left = false
-	_sprite.flip_h =  _sprites_face_left
+	_sprite.flip_h = _sprites_face_left
 	if _hit_box:
 		_hit_box.scale.x = -1 if _sprites_face_left else 1
 	changed_direction.emit(_is_facing_left)
 
-func run(direction : float):
-	if _is_dead || _is_attacking:
+
+func run(direction: float):
+	if _is_dead or (_stop_moving and _is_attacking):
 		_direction = 0
 	else:
 		_direction = direction
+
 
 func jump():
 	if _is_dead || _is_attacking:
@@ -145,13 +172,15 @@ func jump():
 		velocity.y = _jump_velocity
 		_spawn_dust(_jump_dust)
 
+
 func stop_jump():
 	if _is_dead || _is_attacking:
 		return
 	if velocity.y < 0 && not _is_in_water:
 		velocity.y = 0
 
-func enter_water(water_surface_height : float):
+
+func enter_water(water_surface_height: float):
 	_water_surface_height = water_surface_height
 	_is_in_water = true
 	_is_below_surface = false
@@ -159,11 +188,14 @@ func enter_water(water_surface_height : float):
 	if velocity.y > 0:
 		velocity.y *= _drag
 
+
 func exit_water():
 	_is_in_water = false
 
+
 func dive():
 	_is_below_surface = true
+
 
 func revive():
 	_is_dead = false
@@ -174,13 +206,16 @@ func revive():
 	landed.emit(global_position.y)
 	health_changed.emit(float(_current_health) / _max_health)
 
+
 #endregion
+
 
 func _attack_interrupted():
 	_is_attacking = false
 	_hit_box.monitoring = false
 
-func _physics_process(delta : float):
+
+func _physics_process(delta: float):
 	if not _is_facing_left && sign(_direction) == -1:
 		face_left()
 	elif _is_facing_left && sign(_direction) == 1:
@@ -199,7 +234,8 @@ func _physics_process(delta : float):
 		position.x = clamp(position.x, _min.x, _max.x)
 		position.y = clamp(position.y, _min.y, _max.y)
 
-func _ground_physics(delta : float):
+
+func _ground_physics(delta: float):
 	# decelerate to zero
 	if _direction == 0:
 		velocity.x = move_toward(velocity.x, 0, _deceleration * delta)
@@ -210,12 +246,14 @@ func _ground_physics(delta : float):
 	else:
 		velocity.x = move_toward(velocity.x, _direction * _speed, _deceleration * delta)
 
-func _air_physics(delta : float):
+
+func _air_physics(delta: float):
 	velocity.y += gravity * delta
 	if _direction:
 		velocity.x = move_toward(velocity.x, _direction * _speed, _acceleration * _air_control * delta)
 
-func _water_physics(delta : float):
+
+func _water_physics(delta: float):
 	if _direction == 0:
 		velocity.x = move_toward(velocity.x, 0, _deceleration * _drag * delta)
 	else:
@@ -227,14 +265,17 @@ func _water_physics(delta : float):
 	else:
 		velocity.y = move_toward(velocity.y, gravity * _density * _drag * -1, gravity * _drag * delta)
 
+
 func _land():
 	landed.emit(position.y)
 
-func _spawn_dust(dust : PackedScene):
+
+func _spawn_dust(dust: PackedScene):
 	var _dust = dust.instantiate()
 	_dust.position = position
 	_dust.flip_h = _sprite.flip_h
 	get_parent().add_child(_dust)
+
 
 func _die():
 	_is_dead = true
@@ -244,6 +285,7 @@ func _die():
 	collision_mask = 1
 	_direction = 0
 
-func _on_hit_box_area_entered(area : Area2D):
+
+func _on_hit_box_area_entered(area: Area2D):
 	if not _is_dead && _is_attacking:
 		area.get_parent().take_damage(_attack_damage, (area.global_position - global_position).normalized())
--- Scripts/Characters/hero.gd
+++ Scripts/Characters/hero.gd
@@ -1,10 +1,11 @@
 class_name Hero extends Character
 
-@export var _has_sword : bool
-@onready var _attack_input_buffer : Timer = $HitBox/InputBuffer
+@export var _has_sword: bool
+@onready var _attack_input_buffer: Timer = $HitBox/InputBuffer
 @warning_ignore("unused_private_class_variable")
-@onready var _cooldown : Timer = $HitBox/Cooldown
-var _sword : RigidBody2D
+@onready var _cooldown: Timer = $HitBox/Cooldown
+var _sword: RigidBody2D
+
 
 func attack():
 	_wants_to_attack = true
@@ -12,12 +13,15 @@ func attack():
 	await _attack_input_buffer.timeout
 	_wants_to_attack = false
 
+
 func can_equip_sword() -> bool:
 	return not _has_sword && not _is_dead
 
-func equip_sword(sword : RigidBody2D):
+
+func equip_sword(sword: RigidBody2D):
 	_sword = sword
 	_has_sword = true
+
 
 func drop_sword():
 	if not _has_sword:
@@ -26,20 +30,24 @@ func drop_sword():
 	_sword.be_dropped(global_position)
 	_sword = null
 
-func _air_physics(delta : float):
+
+func _air_physics(delta: float):
 	if _is_attacking && velocity.y > 0:
 		velocity.y = 0
 	else:
 		super._air_physics(delta)
 
+
 func _die():
 	if _has_sword:
 		drop_sword()
 	super._die()
-	
-func _on_hit_box_area_entered(area : Area2D):
+
+
+func _on_hit_box_area_entered(area: Area2D):
 	if _is_dead || not _is_attacking:
 		return
 	if not is_on_floor() && area.global_position.y > global_position.y:
 		velocity.y = _jump_velocity / 2
+
 	super._on_hit_box_area_entered(area)
